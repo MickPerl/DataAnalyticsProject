6fd092a9509035f7594c4ca636a86434be9e42da
Emanuele SINAGRA <emanuele.sinagra@alstomgroup.com>
2021-12-25 22:59:55 +0100

    data transformation
    study row from 20 to 24

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from sklearn.preprocessing import MultiLabelBinarizer
 import pandas as pd
 from os import path
 
 pd.set_option('display.max_columns', None)
+# all columns in one line
+pd.set_option('expand_frame_repr', False)
 
 csvPath = "C:\\Users\\esinagra\\Desktop\\Nuova cartella\\Magistrale\\2\\DATA ANALYTICS\\progetto\\ml-25m"
 files = ["movies.csv", "ratings.csv", "genome-scores.csv", "genome-tags.csv", "links.csv", "tags.csv"]
@@ -13,12 +16,15 @@ df_genome_tags = pd.read_csv(path.join(csvPath, files[3]))
 #df_links = pd.read_csv(path.join(csvPath, files[4]))
 #df_tags = pd.read_csv(path.join(csvPath, files[5]))
 
+#df = df_movies['genres'].str.split('|', expand=True)
+mlb = MultiLabelBinarizer(sparse_output=True)
+df_movies = df_movies.join(pd.DataFrame.sparse.from_spmatrix(
+                mlb.fit_transform(df_movies.pop('genres').str.split('|')),
+                index=df_movies.index,
+                columns=mlb.classes_))
 print(df_movies.head())
-print(df_genome_tags.head())
-#print(df_genome_scores.head())
-#df = df_movies.join(df_genome_scores, on="movieId")
-df = pd.concat([df_movies, df_genome_tags['tag']])
+
+df_genome = pd.merge(df_genome_tags, df_genome_scores, on="tagId")
+df_genome = df_genome.pivot(index='movieId', columns='tag', values="relevance")
+df = pd.merge(df_movies, df_genome, on="movieId")
 print(df.head())
-#df = pd.merge(df_movies, df_genome_scores, on="movieId")
-#print(df.head())
-quit(0)

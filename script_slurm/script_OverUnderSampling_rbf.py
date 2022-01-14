8fd22a0def6b272b6a891fc5f7762f9e51ff8c7d
MickPerl <micheleperlino@gmail.com>
2022-01-14 18:37:15 +0100

    Fixes

--- script_slurm/script_OverUnderSampling_rbf.py
+++ script_slurm/script_OverUnderSampling_rbf.py
@@ -71,8 +71,8 @@ df_train = pd.read_csv("df_train.csv")
 df_test = pd.read_csv("df_test.csv")
 df_val = pd.read_csv("df_val.csv")
 
-min_bin_cardinality = df_train.bin_y.value_counts().min()
-df_trains = MultipleRandomUnderSampling(df_train, min_bin_cardinality, n_samples=12)
+
+df_trains = MultipleRandomUnderSampling(df_train, 2000, n_samples=12)
 
 size_C_range = 8
 size_gamma_range = 8
@@ -134,4 +134,4 @@ for c in C_range:
 		}, ignore_index=True)
 
 
-results.to_csv("results_undersampling_rbf.csv", index=False)
+results.to_csv("results_overundersampling_rbf.csv", index=False)
